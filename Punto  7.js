1dc17c6e85e3dffc2bf027ec168ea7dda41e326d
PedroAntonioSanchezYepesADSO101~ <yepeseyt2@gmail.com>
2022-10-13 22:44:15 -0500

    punto 7

---
+++ Punto  7.js
@@ -0,0 +1,6 @@
+let martin = fraction(2/3)
+   let jairo = (1/4)
+   let suma = (martin+jairo)
+   let lorena = (1/1)-suma
+   console.log(`Jairo y martin tienen un total de ${suma}`);
+   console.log(`A lorena le corresponde un total de ${lorena}`);
